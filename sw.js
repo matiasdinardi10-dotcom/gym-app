9d6bdf6cf7425cf08b0feacbddddd8801a2f12ec
matiasdinardi10-dotcom <matiasdinardi10@gmail.com>
2026-05-04 17:51:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,18 @@
-const CACHE_NAME = 'md-pro-training-v2';
-const ASSETS = [
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'md-pro-training-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
